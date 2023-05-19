a56ea73d14d9fd8066a5332c2e2985028a409c49
Pham Bao Phuc <123phuc27602@gmail.com>
2023-05-19 11:06:45 +0700

    search bar, form login,forgot password

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,3 +9,21 @@ window.onresize = function(){
   if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
   else siteBrand.innerHTML = "MY AWESOME WEBSITE"
 }
+
+// trạng thái click vào thanh search
+document.addEventListener("DOMContentLoaded", function() {
+    const searchbar = document.querySelector(".searchbar");
+    const searchInput = document.querySelector(".search_input");
+
+    searchbar.addEventListener("click", function() {
+        searchbar.classList.add("active");
+        searchInput.focus();
+    });
+
+    document.addEventListener("click", function(e) {
+        const isSearchbarClicked = searchbar.contains(e.target);
+        if (!isSearchbarClicked) {
+            searchbar.classList.remove("active");
+        }
+    });
+});
