06d4d2f94b31d397013e8f74ccefb07dcddb3c58
Pham Bao Phuc <123phuc27602@gmail.com>
2023-05-19 21:57:01 +0700

    Datatables product

--- src/main/java/DoAnJava/LinhKienDienTu/controller/AdminController.java
+++ src/main/java/DoAnJava/LinhKienDienTu/controller/AdminController.java
@@ -1,15 +1,31 @@
 package DoAnJava.LinhKienDienTu.controller;
 
+import DoAnJava.LinhKienDienTu.entity.Product;
+import DoAnJava.LinhKienDienTu.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping
     public String index() {
         return "admin/index";
+    }
+
+    @GetMapping("/product")
+    public String getAllProduct(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "admin/m-product";
     }
 }
