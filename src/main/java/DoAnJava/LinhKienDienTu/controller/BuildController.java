99bdee377bc38c8779868c95c6d79f3bc8c4af60
Dương Hoàng Thái <d.thai3009@gmail.com>
2023-06-14 21:15:39 +0700

    build

--- src/main/java/DoAnJava/LinhKienDienTu/controller/BuildController.java
+++ src/main/java/DoAnJava/LinhKienDienTu/controller/BuildController.java
@@ -1,7 +1,6 @@
 package DoAnJava.LinhKienDienTu.controller;
 
 import DoAnJava.LinhKienDienTu.services.CategoryService;
-import DoAnJava.LinhKienDienTu.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BuildController {
 
     @Autowired
-    private ProductService productService;
-    @Autowired
     private CategoryService categoryService;
 
     @GetMapping
@@ -22,5 +19,4 @@ public class BuildController {
         model.addAttribute("categories", categoryService.getAllCategory());
         return "build/index";
     }
-
 }
